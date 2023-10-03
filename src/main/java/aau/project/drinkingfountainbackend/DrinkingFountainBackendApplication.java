b1cd56e9b3cb0fd6248cd8384e8644bc9fa71a9f
Da9el00 <danielsmidstrup1@gmail.com>
2023-10-03 11:56:22 +0200

    First Spring boot base project commit

---
+++ src/main/java/aau/project/drinkingfountainbackend/DrinkingFountainBackendApplication.java
@@ -0,0 +1,13 @@
+package aau.project.drinkingfountainbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkingFountainBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrinkingFountainBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/aau/project/drinkingfountainbackend/DrinkingFountainBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package aau.project.drinkingfountainbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkingFountainBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
