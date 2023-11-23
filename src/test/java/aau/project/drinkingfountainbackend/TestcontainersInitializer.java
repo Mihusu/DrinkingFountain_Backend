711dd4c34fa3d48f942921993d7131949b9bad1e
Da9el00 <danielsmidstrup1@gmail.com>
2023-11-23 12:57:03 +0100

    Missing IT file

---
+++ src/test/java/aau/project/drinkingfountainbackend/TestcontainersInitializer.java
@@ -0,0 +1,26 @@
+package aau.project.drinkingfountainbackend;
+
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+class TestcontainersInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(DockerImageName.parse("postgres:16"))
+            .withInitScript("./tables.sql");
+
+    static {
+        postgres.start();
+    }
+
+    @Override
+    public void initialize(ConfigurableApplicationContext ctx) {
+        TestPropertyValues.of(
+                "spring.datasource.url=" + postgres.getJdbcUrl(),
+                "spring.datasource.username=" + postgres.getUsername(),
+                "spring.datasource.password=" + postgres.getPassword()
+        ).applyTo(ctx.getEnvironment());
+    }
+}
