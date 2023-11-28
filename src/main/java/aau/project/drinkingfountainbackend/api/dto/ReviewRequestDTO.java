4b930c3509752b9d3fc0a3978b168dd563e65283
Da9el00 <danielsmidstrup1@gmail.com>
2023-11-28 13:07:16 +0100

    sonarCube issues

--- src/main/java/aau/project/drinkingfountainbackend/api/dto/ReviewRequestDTO.java
+++ src/main/java/aau/project/drinkingfountainbackend/api/dto/ReviewRequestDTO.java
@@ -1,8 +1,6 @@
 package aau.project.drinkingfountainbackend.api.dto;
 
 import aau.project.drinkingfountainbackend.persistence.entity.DrinkingFountainEntity;
-
-import java.time.ZonedDateTime;
 import java.util.List;
 
 public record ReviewRequestDTO(
